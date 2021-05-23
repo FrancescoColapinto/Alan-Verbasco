65dc797e7e2075303ec0d43eb00e6b09b4cd7322
Francesco Colapinto <francesco.colapinto90@gmail.com>
2021-05-23 20:06:53 +0200

    Agggiunto footer

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,16 +1,44 @@
+$(document).ready(function () {
+    $(".shortcut").animate({
+        width: "toggle"
+    }, 1000);
+    $(".logoname").fadeIn(2000);
+    $(".geo").mouseenter(function(){
+        $(".image").animate({
+            padding: "0"
+        },500)
+    })
+    $(".geo").mouseleave(function(){
+        $(".image").animate({
+            padding: "20px"
+        },500)
+    })
+    $(".geo2").mouseenter(function(){
+        $(".image2").animate({
+            padding: "0"
+        },500)
+    })
+    $(".geo2").mouseleave(function(){
+        $(".image2").animate({
+            padding: "20px"
+        },500)
+    })
+});
+
+
 const sliders = document.querySelectorAll(".slide-in");
 
 
 
 const appearOptions = {
     threshold: 0,
-    rootMargin: "0px 0px -500px 0px"
+    rootMargin: "300px 0px -250px 300px"
 }; 
 
 const appearOnScroll=new IntersectionObserver(function(
     entries,
     appearOnScroll
-) {
+){
     entries.forEach(entry => {
         if (!entry.isIntersecting) {
             return;
