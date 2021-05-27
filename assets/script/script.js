dc15ec301fddd2bc14c5b018d25289a8e67ce7df
Francesco Colapinto <francesco.colapinto90@gmail.com>
2021-05-27 22:35:06 +0200

    Modifiche
    Ordinato e commentato CSS
    Riposizionati gli ultimi due paragrafi

--- assets/script/script.js
+++ assets/script/script.js
@@ -23,27 +23,46 @@ $(document).ready(function () {
             padding: "20px"
         },500)
     })
+    $(".geo").mouseenter(function(){
+        $(".image3").animate({
+            padding: "0"
+        },500)
+    })
+    $(".geo").mouseleave(function(){
+        $(".image3").animate({
+            padding: "20px"
+        },500)
+    })
+    $(".geo2").mouseenter(function(){
+        $(".image4").animate({
+            padding: "0"
+        },500)
+    })
+    $(".geo2").mouseleave(function(){
+        $(".image4").animate({
+            padding: "20px"
+        },500)
+    })
 });
 
+/*slider dei paragrafi*/
+const sliders = document.querySelectorAll(".slide-in"); /*seleziono tutti i miei paragrafi con classe slide-in*/
 
-const sliders = document.querySelectorAll(".slide-in");
-
-
-
-const appearOptions = {
+const appearOptions = { /*creo una costante con i parametri di threshold e root margin da inserire in IntersectionObserver*/
     threshold: 0,
-    rootMargin: "300px 0px -250px 300px"
+    rootMargin: "0px 0px -250px 0px"
+    /*il root margin è impostato per dirmi che appena supero i 250px di visibilità dell'elemento parte la funzione di callback*/
 }; 
 
-const appearOnScroll=new IntersectionObserver(function(
-    entries,
+const appearOnScroll=new IntersectionObserver(function( /*costante principale del mio obiettivo*/
+    entries, /*callback f*/
     appearOnScroll
 ){
     entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            entry.target.classList.add("appear");
+        if (!entry.isIntersecting) { /*se l'elemento desiderato non "compare"*/
+            return;/*non fare niente*/
+        } else { 
+            entry.target.classList.add("appear"); /*inserisci la classe appear*/
             appearOnScroll.unobserve(entry.target);
         }
     })
@@ -51,5 +70,5 @@ const appearOnScroll=new IntersectionObserver(function(
 appearOptions);
 
 sliders.forEach(slider =>{
-    appearOnScroll.observe(slider);
+    appearOnScroll.observe(slider); /*attivo appearOnscroll su sliders, ovvero i miei .slide-in*/
 });
