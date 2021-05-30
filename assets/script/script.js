31366ef367ab27067de69340fc35aca5e26c27a1
Francesco Colapinto <francesco.colapinto90@gmail.com>
2021-05-30 02:10:16 +0200

    Aggiunta animazioni paragrafi
    Ho aggiunto lo slide con wow.js ed ho anche aggiustato la navbar per la mediaquery sotto gli 800px

--- assets/script/script.js
+++ assets/script/script.js
@@ -45,33 +45,6 @@ $(document).ready(function () {
     })
     $(".navbtn").click(function(){
         $(".shortcut li").css("display", "inline")
-        $("nav").css("display", "block")
+        $("nav").toggle(500)
     });
-});
-/*slider dei paragrafi*/
-const sliders = document.querySelectorAll(".slide-in"); /*seleziono tutti i miei paragrafi con classe slide-in*/
-
-const appearOptions = { /*creo una costante con i parametri di threshold e root margin da inserire in IntersectionObserver*/
-    threshold: 0,
-    rootMargin: "0px 0px -250px 0px"
-    /*il root margin è impostato per dirmi che appena supero i 250px di visibilità dell'elemento parte la funzione di callback*/
-}; 
-
-const appearOnScroll=new IntersectionObserver(function( /*costante principale del mio obiettivo*/
-    entries, /*callback f*/
-    appearOnScroll
-){
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) { /*se l'elemento desiderato non "compare"*/
-            return;/*non fare niente*/
-        } else { 
-            entry.target.classList.add("appear"); /*inserisci la classe appear*/
-            appearOnScroll.unobserve(entry.target);
-        }
-    })
-}, 
-appearOptions);
-
-sliders.forEach(slider =>{
-    appearOnScroll.observe(slider); /*attivo appearOnscroll su sliders, ovvero i miei .slide-in*/
 });
